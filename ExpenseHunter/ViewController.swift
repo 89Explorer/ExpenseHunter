b6114e8f32ba265dd6b6f9c51f428ec02f76e651
89Explorer <https://github.com/89Explorer>
2025-07-11 01:48:30 +0900

    feat: 코드로 구현하기 위한 설정 완료

--- ExpenseHunter/ViewController.swift
+++ ExpenseHunter/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        view.backgroundColor = .secondarySystemBackground
+    }
 
 }
 
